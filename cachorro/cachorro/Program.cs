b482ba0d55609d3a1572fece2589ae981571d732
Rafael Guiterrez <111995483+rafawga@users.noreply.github.com>
2023-05-22 08:29:43 -0300

    Update Program.cs

--- cachorro/cachorro/Program.cs
+++ cachorro/cachorro/Program.cs
@@ -1,22 +1,134 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.ComponentModel;
+Thread.Sleep(1000);
+Dog[] Dogs = criarCachorros();
+Pessoa[] Pessoas = criarPessoas();
+int tamanho = 50;
 
-namespace cachorro
+string[] pos = new string[tamanho];
+
+for (int a = 0; a < pos.Length - 1; a++)
 {
-    internal static class Program
+    pos[a] = " ";
+}
+pos[tamanho - 1] = "|CHEGADA|";
+
+
+Random Random = new Random();
+bool finished = true;
+
+while (finished)
+{
+    Thread.Sleep(200);
+    Console.Clear();
+
+    for (int i = 0; i < Dogs.Length; i++)
     {
-        /// <summary>
-        /// Ponto de entrada principal para o aplicativo.
-        /// </summary>
-        [STAThread]
-        static void Main()
+
+
+        if (Dogs[i].pos == tamanho)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Dogs[i].winner = true;
+            finished = false;
         }
+
+        int prob = Random.Next(1, 101);
+
+        if (prob > 50)
+        {
+            Dogs[i].pos += 1;
+        }
+
+        for (int j = 0; j < pos.Length; j++)
+        {
+            if (j == Dogs[i].pos)
+            {
+                Console.Write(Dogs[i].nome);
+            }
+
+            else
+            {
+                Console.Write(pos[j]);
+            }
+
+        }
+
+
+        Console.WriteLine();
+
     }
+}
+
+Console.Clear();
+foreach (var item in Dogs)
+{
+    if (item.winner)
+    {
+        Console.WriteLine("PARABENSSSSSSSS");
+        Console.WriteLine($"CHORRO {item.nome} VENCEUUUUU");
+    }
+}
+
+
+Pessoa[] criarPessoas()
+{
+    Pessoa[] vetorPessoa = new Pessoa[3];
+
+    Pessoa pessoa1 = new Pessoa();
+    pessoa1.nome = "Joao";
+    pessoa1.dinheiro = 50;
+
+    Pessoa pessoa2 = new Pessoa();
+    pessoa2.nome = "Lucas";
+    pessoa2.dinheiro = 75;
+
+    Pessoa pessoa3 = new Pessoa();
+    pessoa3.nome = "Pedro";
+    pessoa3.dinheiro = 100;
+
+    vetorPessoa[0] = pessoa1;
+    vetorPessoa[1] = pessoa2;
+    vetorPessoa[2] = pessoa3;
+
+    return vetorPessoa;
+}
+Dog[] criarCachorros()
+{
+
+    Dog[] vetorDog = new Dog[4];
+
+    Dog dog1 = new Dog();
+    dog1.nome = "R";
+    Dog dog2 = new Dog();
+    dog2.nome = "V";
+    Dog dog3 = new Dog();
+    dog3.nome = "S";
+    Dog dog4 = new Dog();
+    dog4.nome = "M";
+
+
+    vetorDog[0] = dog1;
+    vetorDog[1] = dog2;
+    vetorDog[2] = dog3;
+    vetorDog[3] = dog4;
+
+
+    return vetorDog;
+}
+
+
+public class Pessoa
+{
+    public string nome { get; set; }
+    public int dinheiro { get; set; }
+}
+
+
+public class Dog
+{
+    public int acel { get; set; }
+    public int max { get; set; }
+    public int pos { get; set; } = 0;
+    public string nome { get; set; }
+    public bool winner { get; set; } = false;
+
 }
